408ae550f475128db37ffa19f4a4f2c4049a9612
Meekah1 <bolarinwa_akeem@yahoo.com>
2023-06-07 21:02:03 +0100

    Chat page Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 // import { useState } from 'react'
-import {Routes} from "react-router-dom";
-import { MainRoutes } from './routes/index';
-import { UserDashboardRoutes } from './routes/dashboard/user.jsx';
-import { CompanyDashboardRoutes } from './routes/dashboard/company.jsx';
+// import {Routes} from "react-router-dom";
+// import { MainRoutes } from './routes/index';
+// import { UserDashboardRoutes } from './routes/dashboard/user.jsx';
+// import { CompanyDashboardRoutes } from './routes/dashboard/company.jsx';
+import ChatPage from './pages/user_dashboard/ChatPage';
 
 function App() {
   return (
     <>
-        <MainRoutes />
+        {/* <MainRoutes />
         <UserDashboardRoutes />
         <CompanyDashboardRoutes />
+         */}
+         <ChatPage />
     </>
   );
 }
